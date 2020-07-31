d2e90f143866b88baac4b946e36d03463e3255cc
Oliveia-86 <egberto.duarte.14@gmail.com>
2020-07-31 16:57:38 -0300

    Saving a new Department

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Seller implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	private String email;
@@ -105,7 +105,4 @@ public class Seller implements Serializable {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-	
-	
-	
 }
